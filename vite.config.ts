298741bb239c91bcef51b64f3ebbea844e3a2837
muzea <mr.muzea@gmail.com>
2023-03-10 01:45:34 +0800

    fix: react import error

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,37 @@
 import { defineConfig } from "vite";
+import externalGlobals from "rollup-plugin-external-globals";
+// import { visualizer } from "rollup-plugin-visualizer";
+// @ts-ignore
 import createExternal from "vite-plugin-external";
 
 export default defineConfig({
   root: "src",
   build: {
+    target: "chrome94",
     outDir: "../dist",
-    // rollupOptions: {
-    //   external: ['react', 'react-dom'],
-    //   output: [
-    //     {
-    //       globals: { react: 'React', 'react-dom': 'ReactDOM' }
-    //     }
-    //   ]
-    // },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: [
+        {
+          globals: { react: "React", "react-dom": "ReactDOM" },
+        },
+      ],
+      plugins: [
+        externalGlobals({
+          react: "React",
+          "react-dom": "ReactDOM",
+        }),
+        // visualizer({ emitFile: true, open: true }),
+      ],
+    },
   },
   plugins: [
     createExternal({
-      externals: {
-        react: "React",
-        "react-dom": "ReactDOM",
+      development: {
+        externals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
       },
     }),
   ],
